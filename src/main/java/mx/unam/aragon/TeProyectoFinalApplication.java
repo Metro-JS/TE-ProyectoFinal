46bcbeb0da0a94aaab902cca3396c63b281b3f97
Jorge Jiménez <69277648+Metro-JS@users.noreply.github.com>
2025-06-04 01:00:07 -0600

    Update and rename TeProyectoFinalJorge to TeProyectoFinal

--- src/main/java/mx/unam/aragon/TeProyectoFinalApplication.java
+++ src/main/java/mx/unam/aragon/TeProyectoFinalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeProyectoFinalJorgeApplication {
+public class TeProyectoFinalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TeProyectoFinalJorgeApplication.class, args);
+        SpringApplication.run(TeProyectoFinalApplication.class, args);
     }
 
 }
