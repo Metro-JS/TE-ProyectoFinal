fa0cd3071882a90bde2ee518c6c4be1c7eb1616a
Metro-JS <mjs.proyectos@gmail.com>
2025-06-05 01:46:14 -0600

    Se agrega controlador basico para josue, ademas se agregan las ligas en las cards para cada uno de los proyectos. Listo para bifurcacion

--- src/main/java/mx/unam/aragon/controller/JosueController.java
+++ src/main/java/mx/unam/aragon/controller/JosueController.java
@@ -1,9 +1,15 @@
 package mx.unam.aragon.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/josue")
 public class JosueController{
+    @GetMapping("")
+    public String josue(Model model) {
+        return "josue/josue-index";
+    }
 }
