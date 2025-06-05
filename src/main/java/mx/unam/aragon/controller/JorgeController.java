ab6676b983ae77ac80204f13207c3a45f0171ea3
Metro-JS <mjs.proyectos@gmail.com>
2025-06-04 21:35:47 -0600

    cambios esteticos

---
+++ src/main/java/mx/unam/aragon/controller/JorgeController.java
@@ -0,0 +1,15 @@
+package mx.unam.aragon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jorge")
+public class JorgeController {
+    @GetMapping("")
+    public String jorge(Model model) {
+        return "jorge/jorge-index";
+    }
+}
---
+++ src/main/java/mx/unam/aragon/controller/JosueController.java
@@ -0,0 +1,9 @@
+package mx.unam.aragon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/josue")
+public class JosueController{
+}
