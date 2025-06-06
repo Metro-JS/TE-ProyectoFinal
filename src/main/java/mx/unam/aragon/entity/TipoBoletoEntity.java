4ae0bc89f629ac4e6aa09aab21d6a72eae33eecb
Metro-JS <mjs.proyectos@gmail.com>
2025-06-05 18:05:22 -0600

    Formulario para tipo de boleto con validaciones y todo funcionando

--- src/main/java/mx/unam/aragon/entity/TipoBoletoEntity.java
+++ src/main/java/mx/unam/aragon/entity/TipoBoletoEntity.java
@@ -1,6 +1,8 @@
 package mx.unam.aragon.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,8 +20,11 @@ public class TipoBoletoEntity {
     private Long id;
 
     @Column(name = "nombre")
+    @NotBlank(message = "Nombre no debe estar en blanco")
+    @Size(max = 50, message = "El nombre no debe ser mayor a 50 caracteres")
     private String nombre;
 
     @Column(name = "descripcion")
+    @NotBlank(message = "La descripcion no debe estar vacia")
     private String descripcion;
 }
