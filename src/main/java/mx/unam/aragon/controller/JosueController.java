31101d924f28427982c4cd087e9dd1a46435267b
Mikasa500 <josue.hernandez.rosas246@gmail.com>
2025-06-05 21:21:36 -0600

    Terminado el poder modificar los datos de los dinosaurios

--- src/main/java/mx/unam/aragon/controller/JosueController.java
+++ src/main/java/mx/unam/aragon/controller/JosueController.java
@@ -18,7 +18,7 @@ public class JosueController{
     @Autowired
     DinosaurioService dinosaurioService;
 
-    @GetMapping("josue-index")
+    @GetMapping("/josue-index")
     public String listaDinosaurio(Model model){
         List<DinosaurioEntity> lista=dinosaurioService.findAll();
         model.addAttribute("lista",lista);
@@ -26,30 +26,30 @@ public class JosueController{
         return "josue/josue-index";
     }
 
-    @GetMapping("agregar-dinosaurio")
+    @GetMapping("/agregar-dinosaurio")
     public String agregarDinosaurio(Model model){
         DinosaurioEntity dinosaurio=new DinosaurioEntity();
         model.addAttribute("dinosaurio",dinosaurio);
         model.addAttribute("contenido","Añadir Nuevo Dinosaurio");
-        return "josue/agregar-dinosaurio";
+        return "/josue/agregar-dinosaurio";
     }
 
-    @PostMapping("guardar-dinosaurio")
+    @PostMapping("/guardar-dinosaurio")
     public String guardarDinosaurio(@Valid @ModelAttribute(value = "dinosaurio") DinosaurioEntity dinosaurio,
                                     BindingResult result, Model model){
         if (result.hasErrors()) {
             for (ObjectError error : result.getAllErrors()) {
                 System.out.println("Error: " + error.getDefaultMessage());
             }
-            return "josue/agregar-dinosaurio";
+            return "/josue/agregar-dinosaurio";
         }
         //realizar la lògica de negocio
         dinosaurioService.save(dinosaurio);
         model.addAttribute("contenido","Se almaceno con exito");
-        return "josue/agregar-dinosaurio";
+        return "/josue/agregar-dinosaurio";
     }
 
-    @GetMapping("eliminar-dinosaurio/{id}")
+    @GetMapping("/eliminar-dinosaurio/{id}")
     public String delete(@PathVariable("id")Long id,
                          Model model){
         dinosaurioService.deleteById(id);
@@ -57,12 +57,12 @@ public class JosueController{
 
     }
 
-    @GetMapping("modificar-dinosaurio/{id}")
+    @GetMapping("/modificar-dinosaurio/{id}")
     public String modificar(@PathVariable("id")Long id,
                            Model model){
         DinosaurioEntity dinosaurio=dinosaurioService.findById(id);
         model.addAttribute("dinosaurio",dinosaurio);
         model.addAttribute("contenido","Alterar datos del dinosaurio");
-        return "josue/agregar-dinosaurio";
+        return "/josue/agregar-dinosaurio";
     }
 }
