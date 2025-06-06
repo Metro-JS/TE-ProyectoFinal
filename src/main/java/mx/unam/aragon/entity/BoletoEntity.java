80be170e9d92c492ec9a24f23765bfc0cb3abdb6
Metro-JS <mjs.proyectos@gmail.com>
2025-06-05 20:28:53 -0600

    alta de boletos

--- src/main/java/mx/unam/aragon/entity/BoletoEntity.java
+++ src/main/java/mx/unam/aragon/entity/BoletoEntity.java
@@ -1,6 +1,9 @@
 package mx.unam.aragon.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,9 +22,13 @@ public class BoletoEntity {
     private Long id;
 
     @Column(name = "anyo")
+    @Min(value = 1900, message = "el anyo debe tener un formato numerico YYYY, entre 1900 y 2030")
+    @Max(value = 2030, message = "el anyo debe tener un formato numerico YYYY, entre 1900 y 2030")
+    @NotNull(message = "anyo no debe ser vacio")
     private Integer anyo;
 
     @Column(name = "lote")
+
     private String lote;
 
     @Column(name = "folio")
@@ -29,6 +36,9 @@ public class BoletoEntity {
 
     @Column(name = "urlfoto")
     private String urlfoto;
+
+    @Column(name = "numero")
+    private Integer numero;
 
     @ManyToOne
     @JoinColumn(name = "idtipo", nullable = false)
